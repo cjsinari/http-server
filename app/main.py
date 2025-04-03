4d6f3a7befa847249ebcf7db644083f4a1c51524
cjsinari <cyrilsinari@gmail.com>
2025-04-03 23:49:55 +0300

    Respond request with body

--- app/main.py
+++ app/main.py
@@ -10,15 +10,30 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.listen()
 
+while True: # Allow multiple connections
     client, addr = server_socket.accept()
+    print(f"Accepted connection from{addr}")
     
     client_msg = client.recv(4096).decode().split(" ")
-    if client_msg[1] == "/":
+    print(f"Received request: {client_msg}")  #Debugging output
+    
+    if len (client_msg) < 2:
+        client.sendall(b"HTTP/1.1 400 Request failed\r\n\r\n")
+        client.close()
+        continue
+       
+    request_path = client_msg[1]
+
+    if request_path == "/":
        client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    elif client.startswith("/echo/")[1]:   
-       client.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}".encode())
+    elif request_path.startswith("/echo/"):   
+       value = request_path[6:]  #Extract text after "/echo"
+       response = f"HTTP/1.1 200 OK \r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}"
+       client.sendall(response.encode())
     else: 
         client.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+    client.close()
 
 
 if __name__ == "__main__":

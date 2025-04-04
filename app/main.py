802060372f26c8e750989090fd4a8752822e742b
cjsinari <cyrilsinari@gmail.com>
2025-04-05 00:03:59 +0300

    Returning requested file to client

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import socket  # noqa: F401
 import threading
-
+import argparse
+import os
 
 def handle_client(client):
     #Handles a single client request in a separate thread
@@ -31,8 +32,31 @@ def handle_client(client):
           response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}" 
           client.sendall(response.encode())
 
+        #Extracting file path from OS and reading Content-Length:
+        elif request_path.startwith("/files/"):
+            filename = request_path[len("/files/"):]
+            filepath = os.path.join(FILES_DIRECTORY, filename)
+            if os.path.isfile(filepath):
+                try:
+                    with open(filepath, "rb") as f:
+                        content = f.read()
+                    response_headers = (
+                        "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: application/octet-stream\r\n"
+                        f"Content-Length: {len(content)}\r\n"
+                        "\r\n"
+                    )  
+                    client.sendall(response_headers.encode() +content)  
+
+                except Exception as e:
+                    print(f"Error reading file:{e}")
+                    client.sendall(b"HTTP/1.1 500 Internal Server Error\r\n\r\n")     
+
+            else:
+              client.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
         else:
-          client.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            client.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")     
     
     except Exception as e:
         print(f"Error handling client: {e}")      
@@ -41,8 +65,15 @@ def handle_client(client):
 
 
 def main():
+    global FILES_DIRECTORY
 
     print("Logs from your program will appear here!")
+    
+    #Parsing arguments to file directory to extract file
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory", required=True, help="Absolute path to the file directory")
+    args = parser.parse_args()
+    FILES_DIRECTORY = args.directory
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.listen()
